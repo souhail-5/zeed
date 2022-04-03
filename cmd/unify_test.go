a4400bf7845ce33e74899c744b430e0b40e57166
Souhail Moussaoui <souhail@webtasmim.com>
2022-04-03 14:47:05 +0200

    Add a test: TestUnifyWithTemplate

--- cmd/unify_test.go
+++ cmd/unify_test.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"bytes"
 	"fmt"
+	"github.com/spf13/viper"
 	"io/ioutil"
 	"testing"
 )
@@ -45,5 +46,51 @@ func TestUnify(t *testing.T) {
 			t.Fatal(err)
 		}
 		t.Fatalf("Expected %v got %v", 1, len(files))
+	}
+}
+
+func TestUnifyWithTemplate(t *testing.T) {
+	initRepository(t)
+	defer removeRepository(t)
+	viper.Set("channels", []string{"added", "security"})
+	bufferString := bytes.NewBufferString("")
+	rootCmd.SetOut(bufferString)
+	rootCmd.SetArgs([]string{"My changelog entry #1", "-c", "added"})
+	if err := rootCmd.Execute(); err != nil {
+		t.Fatal(err)
+	}
+	rootCmd.SetArgs([]string{
+		"My changelog entry #2",
+		"-p",
+		fmt.Sprintf("%d", 32),
+		"-c",
+		"added",
+	})
+	if err := rootCmd.Execute(); err != nil {
+		t.Fatal(err)
+	}
+	rootCmd.SetArgs([]string{"My changelog entry #3", "-c", "security"})
+	if err := rootCmd.Execute(); err != nil {
+		t.Fatal(err)
+	}
+	rootCmd.SetArgs([]string{"unify", "-t", "keepachangelog"})
+	if err := rootCmd.Execute(); err != nil {
+		t.Fatal(err)
+	}
+
+	out, err := ioutil.ReadAll(bufferString)
+	if err != nil {
+		t.Fatal(err)
+	}
+	expected := `### Added
+- My changelog entry #2
+- My changelog entry #1
+
+### Security
+- My changelog entry #3
+
+`
+	if expected != string(out) {
+		t.Fatalf("Expected %q got %q", expected, string(out))
 	}
 }
